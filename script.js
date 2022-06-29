bb4873e855aa24c14ee9e7c232d7807f8788bb08
Ebenezer-uayson <104830086+Ebenezer-uayson@users.noreply.github.com>
2022-06-29 02:31:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const inputs = document.querySelector(".inputs"),
+resetBtn = document.querySelector(".reset-btn"),
+hint = document.querySelector(".hint span"),
+wrongLetter = document.querySelector(".wrong-letter span"),
+attempts = document.querySelector(".attempts span"),
+typingInput = document.querySelector(".typing-input");
+
+let word,maxGuesses,corrects=[] ,incorrects=[];
+function randomWord(){
+    //getting random object from wordList
+    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+     word = ranObj.word;//getting word of random object
+     maxGuesses = 10; corrects=[]; incorrects=[];
+  
+
+    hint.innerText = ranObj.hint;
+    attempts.innerText = maxGuesses;
+    wrongLetter.innerText = incorrects;
+   
+
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+      html += '<input type="text"  disabled>';
+        
+    }
+    inputs.innerHTML = html;
+
+}
+randomWord();
+//Getting User pressed Value
+function initGame(e){
+    let key = e.target.value;
+    if(key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key}`) && !corrects.includes(key)){
+       
+            // If using letter found in the word
+        if(word.includes(key)){
+          // Showing matched letters in the appropriate input
+          for (let i = 0; i < word.length; i++) {
+              if(word[i] === key){
+                  corrects.push(key);
+                  inputs.querySelectorAll("input")[i].value = key
+              }
+              
+          }
+        }else{
+            maxGuesses--; // decrement of guesses by 1
+            incorrects.push(` ${key}`);
+        }
+    attempts.innerText = maxGuesses;
+    wrongLetter.innerText = incorrects;
+    }
+    typingInput.value = "";
+   setTimeout(() => {
+    if(corrects.length === word.length){ // if user found all letters
+        alert(`Congrats!!, You guessed right ${word.toUpperCase()}`);
+        randomWord();
+    } else if (maxGuesses < 1){ // if user couldn't find all letters
+     alert("Game over!,  You don't have remaining attempts");
+     for (let i = 0; i < word.length; i++) {
+         // showing all letters in the input
+        inputs.querySelectorAll("input")[i].value = word[i];
+    }
+    }
+   });
+}
+
+resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame );
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
---
+++ words.js
@@ -0,0 +1,62 @@
+const wordList = [
+{
+    word: "python",
+    hint: "A programming language"
+},
+{
+    word: "laptop",
+    hint: "An ICT tool"
+},
+{
+    word: "venus",
+    hint: "A planet of our solar system"
+},
+{
+    word: "zoo",
+    hint: "A place where animals are kept"
+},
+{
+    word: "html",
+    hint: "A markup language"
+},
+{
+    word: "aim",
+    hint: "A purpose or intention"
+},
+{
+    word: "guitar",
+    hint: "A musical instrument"
+},
+{
+    word: "jpeg",
+    hint: "A small image file format"
+},{
+    word: "svg",
+    hint: "A vector image format"
+},
+{
+    word: "idea",
+    hint: "A thought or suggestion"
+},{
+    word: "search",
+    hint: "Act to find something"
+},
+{
+    word: "oxygen",
+    hint: "Gas required for survival"
+},{
+    word: "mercury",
+    hint: "The 80th element on the periodic table"
+},
+{
+    word: "acid",
+    hint: "substance that turns blue litmous paper red"
+},{
+    word: "mass",
+    hint: "A base quantity"
+},
+{
+    word: "newtons",
+    hint: "The SI unit of force"
+},
+]
